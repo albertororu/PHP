6c092f4d56b42a3adbce4932831eeaa2771448f2
albertororu <albertororu16@gmail.com>
2024-02-13 14:26:51 +0100

    P5

---
+++ P5 PHP/aplicacion.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aplicacion</title>
+</head>
+<body>
+    <nav>
+        <a href="usuario.php">Crear usuario </a>
+        <a href="modificar.php">Modificar datos </a>
+        <a href="eliminar.php">Eliminar un usuario </a>
+        <a href="logout.php">Cerrar sesion</a>
+    </nav>
+    <h3>Bienvenido, has accedido correctamente a la página.</h3>
+    <h5>Página creada por Alberto Romero Rubiales</h5>
+</body>
+</html>
